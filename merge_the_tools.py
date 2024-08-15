3101be78e07b3b5e92d28fcaa17a3628e4fce23b
Sanya-09 <agarwalsanya90@gmail.com>
2024-08-15 11:50:42 +0530

    Create merge_the_tools.py

---
+++ merge_the_tools.py
@@ -0,0 +1,16 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    c=0
+    s=''
+    for i in string:
+        if i not in s:
+            s=s+i
+        c+=1
+        if(c==k):
+            print(s)
+            c=0
+            s=''
+
+string = str(input("Enter String :- "))
+k = int(input("Enter integer :- "))
+merge_the_tools(string,k)
